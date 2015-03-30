042a7adb0d685ae5cbcbdf61fad743fe48a6347b
Victor Canal <victor.canal@criteriait.com>
2015-03-30 14:51:49 -0500

    Virtual Class

---
+++ VirtualClass/VirtualClass/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VirtualClass.Startup))]
+namespace VirtualClass
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
